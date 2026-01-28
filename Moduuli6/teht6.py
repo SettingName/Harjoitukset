ee151cad14f8c470ced33bf16bac231a9add52d3
SettingName <m.aleksanteri.vahakuopus@gmail.com>
2026-01-28 13:14:10 +0200

    Update teht6.py

--- Moduuli6/teht6.py
+++ Moduuli6/teht6.py
@@ -1,9 +1,8 @@
 import math
 def calculate_unit_price(diameter, price):
-    area = math.pi * (diameter/2)**2
+    area = math.pi * (diameter/2/100)**2
     base_price = price / area
-    value = base_price * (math.sqrt(area))**2
-    return value
+    return base_price
 
 first_pizza_dia = int(input("Enter the diameter of the first pizza (cm): "))
 first_pizza_pri = int(input("Enter the price of the first pizza (euros): "))
@@ -12,7 +11,7 @@ second_pizza_pri = int(input("Enter the price of the second pizza (euros): "))
 
 print(f"Unit price of the first pizza: {calculate_unit_price(first_pizza_dia, first_pizza_pri):.2f} euros/m²")
 print(f"Unit price of the second pizza: {calculate_unit_price(second_pizza_dia, second_pizza_pri):.2f} euros/m²")
-if calculate_unit_price(first_pizza_dia, first_pizza_pri) > calculate_unit_price(second_pizza_dia, second_pizza_pri):
+if calculate_unit_price(first_pizza_dia, first_pizza_pri) < calculate_unit_price(second_pizza_dia, second_pizza_pri):
     print("The first pizza provides better value for money.")
 else:
     print("The second pizza provides better value for money.")
