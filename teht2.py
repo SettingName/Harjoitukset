4b279b2812a641841e896f13b894e15e88872a21
Markus V <m.aleksanteri.vahakuopus@gmail.com>
2026-03-18 09:37:00 +0000

    teht2 lisäys

---
+++ teht2.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed, current_speed = 0, travelled_distance = 0):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+    
+    def accelerate(self, speedChange: int):
+        self.current_speed = min(max((self.current_speed + speedChange), 0), self.maximum_speed)
