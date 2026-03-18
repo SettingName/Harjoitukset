4d04009d9969f368f3c0b0a6f11109c252f70a08
Markus V <m.aleksanteri.vahakuopus@gmail.com>
2026-03-18 09:56:33 +0000

    teht3 lisäys

--- Moduuli9/teht3.py
+++ Moduuli9/teht3.py
@@ -7,3 +7,6 @@ class Car:
     
     def accelerate(self, speedChange: int):
         self.current_speed = min(max((self.current_speed + speedChange), 0), self.maximum_speed)
+
+    def drive(self, time):
+        self.travelled_distance += (self.current_speed * time)
